53cb536016f8435870ffe9a13d5f59fc123f34a1
Mike <leczinm@clarkson.edu>
2016-10-31 20:00:25 -0400

    Test again commit

--- Documents/workspace/JavaProject1/src/application/Main.java
+++ Documents/workspace/JavaProject1/src/application/Main.java
@@ -16,6 +16,11 @@ public class Main extends Application {
 			Scene scene = new Scene(root,400,400);
 			
 			System.out.println("Test 2test");
+			
+			
+			System.out.println("TEST AGAIN");
+			
+			
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
